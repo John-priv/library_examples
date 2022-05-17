402643d20cb42e77967c954868860ce1024bc4c1
John Privitera <johnpriv@live.com>
2022-05-16 23:36:15 -0400

    Make threads in asyncio_add.py run asynchronously

--- python/asyncio/asyncio_add.py
+++ python/asyncio/asyncio_add.py
@@ -1,8 +1,11 @@
 '''
 Simple Asyncio example (adds numbers asynchronously)
 
-NOTE: Currently thread 1 and thread 2 are run sequentially (not concurrently)
-        - Need to fix this so both tasks are running in parallel
+Creates two addition threads that run asynchronously:
+- Addition threads take in a list
+- Adds one element to the thread's sum then sleeps for 1 second
+
+TODO: Add more to this script/setup networking
 '''
 
 import asyncio
@@ -17,6 +20,7 @@ async def addition_thread(numbers_to_add, start_time, thread_id):
     sum = 0
     for number in numbers_to_add:
         sum += number
+        # await asyncio.sleep(1)
         await asyncio.sleep(1)
         print('Thread: {}   -   Current Sum: {}   -   Time: {:.3}'.format(thread_id, sum, time.time() - start_time))
     return sum
@@ -24,8 +28,13 @@ async def addition_thread(numbers_to_add, start_time, thread_id):
 
 async def main():
     start_time = time.time()
-    sum1 = await addition_thread(numbers_to_add=[1, 2, 3], start_time=start_time, thread_id=1)  # Sum is 6
-    sum2 = await addition_thread(numbers_to_add=[4, 5, 6], start_time=start_time, thread_id=2)  # Sum is 15
+    num_threads = 0
+
+    sum1, sum2 = await asyncio.gather(
+        addition_thread(numbers_to_add=[1, 2, 3], start_time=start_time, thread_id=num_threads),  # Sum is 6
+        addition_thread(numbers_to_add=[4, 5, 6], start_time=start_time, thread_id=num_threads+1)  # Sum is 15
+    )
+
     print('sum1 = {}'.format(sum1))
     print('sum2 = {}'.format(sum2))
 
